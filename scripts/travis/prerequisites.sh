d8f497878d11874bb9196f6d705f13f3175be1f4
Mark Tehver <mark.tehver@gmail.com>
2016-08-13 22:26:41 +0300

    Travis

--- scripts/travis/prerequisites.sh
+++ scripts/travis/prerequisites.sh
@@ -1,18 +1,49 @@
-export DIR=`pwd`
-source scripts/travis/cmake.sh;
-cd $DIR;
-source scripts/travis/swig.sh;
-cd $DIR;
-source scripts/travis/boost.sh;
-cd $DIR;
-source scripts/travis/externallibs.sh;
-cd $DIR;
-source scripts/travis/link-dirs.sh;
-cd $DIR
+set -e
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+echo '---- Downloading and installing CMake ----'
+curl -o cmake-3.5.2.tar.gz -L https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz
+rm -rf cmake-3.5.2
+tar xpfz cmake-3.5.2.tar.gz
+cd cmake-3.5.2
+./configure
+make
+sudo make install
+cd ..
+
+echo '---- Downloading and installing Swig ----'
+rm -rf mobile-swig
+git clone https://github.com/CartoDB/mobile-swig.git
+cd mobile-swig
+cd pcre
+aclocal
+automake
+./configure --prefix=`pwd`/pcre-swig-install --disable-shared
+make
+make install
+export PATH=$PATH:/usr/local/bin
+cd ..
+./autogen.sh
+./configure --disable-ccache
+make
+sudo make install || true
+cd ..
+
+echo '---- Downloading and installing boost ----'
+curl -o boost_1_61_0.tar.gz -L https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz
+rm -r -f boost_1_61_0
+tar xpfz boost_1_61_0.tar.gz
+ln -sf ./boost_1_61_0 ./libs-external/boost
+
+echo '---- Downloading and installing External libs ----'
+rm -rf mobile-external-libs
+git clone https://github.com/CartoDB/mobile-external-libs.git
+ln -sf ./mobile-external-libs/libs-external ./libs-external
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  echo '---- Downloading and installing Android NDK r12b ----'
   curl -L http://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip -O
-  unzip android-ndk-r12b-linux-x86_64.zip
+  rm -r -f android-ndk-r12b
+  unzip -q android-ndk-r12b-linux-x86_64.zip
   rm android-ndk-r12b-linux-x86_64.zip
   export ANDROID_NDK_HOME=`pwd`/android-ndk-r12b;
 fi
