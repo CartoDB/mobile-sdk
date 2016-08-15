5820343747b7ceedce7c05b58c714aef5038ba31
Mark Tehver <mark.tehver@gmail.com>
2016-08-15 10:07:12 +0300

    Try to get iOS Travis build running again

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -2,9 +2,9 @@ set -e
 cd scripts
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   python swigpp-java.py
-  python build-android.py --android-abi=armeabi-v7a
+  python build-android.py --android-abi=armeabi-v7a;
 fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   python swigpp-objc.py
-  python build-ios.py --ios-arch=arm64 | egrep '^(/.+:[0-9+:[0-9]+:.(error|warning):|fatal|===)';
+  python build-ios.py --ios-arch=arm64; # | egrep '^(/.+:[0-9+:[0-9]+:.(error|warning):|fatal|===)';
 fi
