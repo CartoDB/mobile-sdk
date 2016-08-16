e5ee92ee8f7515fb6219fd1cc6e36e254dbcd484
Mark Tehver <mark.tehver@gmail.com>
2016-08-16 13:30:40 +0300

    Removed unused scripts

--- scripts/travis/externallibs.sh
+++
@@ -1,5 +0,0 @@
-echo '---- Downloading and installing External libs ----'
-set -e
-rm -rf mobile-external-libs
-git clone https://github.com/CartoDB/mobile-external-libs.git
-export EXTERNALLIBS_DIR=$PWD/mobile-external-libs
