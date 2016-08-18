f16eb3f01cedaa0740a05a7db4588cf446dd01a0
Mark Tehver <mark.tehver@gmail.com>
2016-08-18 13:51:29 +0300

    Removed (again?) unnecessary travis files

--- scripts/travis/boost.sh
+++
@@ -1,6 +0,0 @@
-echo '---- Downloading and installing boost ----'
-set -e
-curl -o boost_1_61_0.tar.gz -L https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz
-rm -r -f boost_1_61_0
-tar xpfz boost_1_61_0.tar.gz
-export BOOST_DIR=$PWD/boost_1_61_0
--- scripts/travis/link-dirs.sh
+++
@@ -1,3 +0,0 @@
-set -e
-ln -sf "$EXTERNALLIBS_DIR/libs-external" ./libs-external
-ln -sf "$BOOST_DIR" ./libs-external/boost
