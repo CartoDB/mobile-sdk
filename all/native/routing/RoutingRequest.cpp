b4d5b9af41282f57b4c480629de2ab79ca396338
mtehver <mark@nutiteq.ee>
2017-01-02 17:21:53 +0200

    Implemented toString for RoutingResult and RoutingRequest

--- all/native/routing/RoutingRequest.cpp
+++ all/native/routing/RoutingRequest.cpp
@@ -3,6 +3,9 @@
 #include "RoutingRequest.h"
 #include "components/Exceptions.h"
 
+#include <iomanip>
+#include <sstream>
+
 namespace carto {
 
     RoutingRequest::RoutingRequest(const std::shared_ptr<Projection>& projection, const std::vector<MapPos>& points) :
@@ -23,6 +26,18 @@ namespace carto {
 
     const std::vector<MapPos>& RoutingRequest::getPoints() const {
         return _points;
+    }
+
+    std::string RoutingRequest::toString() const {
+        std::stringstream ss;
+        ss << std::setiosflags(std::ios::fixed);
+        ss << "RoutingRequest [points=";
+        for (auto it = _points.begin(); it != _points.end(); ++it) {
+            const MapPos& pos = *it;
+            ss << (it == _points.begin() ? "" : ", ") << pos.toString();
+        }
+        ss << "]";
+        return ss.str();
     }
 
 }
