2c909f054769d8b1cf08ad77a46b9348bc7d24a7
mtehver <mark.tehver@gmail.com>
2017-07-02 23:36:37 +0300

    Basic CartoCSS/Mapnik rule optimization

--- libs-carto/mapnikvt/src/mapnikvt/Style.cpp
+++ libs-carto/mapnikvt/src/mapnikvt/Style.cpp
@@ -1,17 +1,15 @@
 #include "Style.h"
 #include "Expression.h"
+#include "Filter.h"
+#include "Predicate.h"
 #include "Rule.h"
+#include "Symbolizer.h"
+
+#include <algorithm>
 
 namespace carto { namespace mvt {
-    Style::Style(std::string name, float opacity, std::string compOp, FilterMode filterMode, std::vector<std::shared_ptr<const Rule>> rules) : _name(std::move(name)), _opacity(opacity), _compOp(std::move(compOp)), _filterMode(filterMode), _rules(std::move(rules)), _zoomRuleMap() {
-        for (auto it = _rules.begin(); it != _rules.end(); it++) {
-            const std::shared_ptr<const Rule>& rule = *it;
-            std::unordered_set<std::shared_ptr<const Expression>> fieldExprs = rule->getReferencedFields();
-            for (int zoom = rule->getMinZoom(); zoom < rule->getMaxZoom(); zoom++) {
-                _zoomRuleMap[zoom].push_back(rule);
-                _zoomFieldExprsMap[zoom].insert(fieldExprs.begin(), fieldExprs.end());
-            }
-        }
+    Style::Style(std::string name, float opacity, std::string compOp, FilterMode filterMode, std::vector<std::shared_ptr<const Rule>> rules) : _name(std::move(name)), _opacity(opacity), _compOp(std::move(compOp)), _filterMode(filterMode), _rules(std::move(rules)) {
+        rebuildZoomRuleMap();
     }
 
     const std::vector<std::shared_ptr<const Rule>>& Style::getZoomRules(int zoom) const {
@@ -30,5 +28,54 @@ namespace carto { namespace mvt {
             return emptyFieldExprs;
         }
         return it->second;
+    }
+
+    void Style::optimizeRules() {
+        if (_filterMode != FilterMode::FIRST) {
+            return;
+        }
+        if (_rules.size() < 1) {
+            return;
+        }
+
+        bool updated = false;
+        for (auto it = _rules.begin(); it + 1 != _rules.end(); ) {
+            std::shared_ptr<const Rule> rule1 = *(it + 0);
+            std::shared_ptr<const Rule> rule2 = *(it + 1);
+            if (rule1->getMinZoom() == rule2->getMinZoom() && rule1->getMaxZoom() == rule2->getMaxZoom() && rule1->getFilter()->getType() == Filter::Type::FILTER && rule2->getFilter()->getType() == Filter::Type::FILTER && rule1->getSymbolizers().size() == rule2->getSymbolizers().size()) {
+                if (std::equal(rule1->getSymbolizers().begin(), rule1->getSymbolizers().end(), rule2->getSymbolizers().begin(), [](const std::shared_ptr<Symbolizer>& symbolizer1, const std::shared_ptr<Symbolizer>& symbolizer2) {
+                    return typeid(symbolizer1.get()) == typeid(symbolizer2.get()) && symbolizer1->getParameterMap() == symbolizer2->getParameterMap();
+                })) {
+                    std::shared_ptr<Predicate> combinedPred;
+                    if (rule1->getFilter()->getPredicate() && rule2->getFilter()->getPredicate()) {
+                        combinedPred = std::make_shared<OrPredicate>(rule1->getFilter()->getPredicate(), rule2->getFilter()->getPredicate());
+                    }
+                    std::shared_ptr<Filter> combinedFilter = std::make_shared<Filter>(Filter::Type::FILTER, combinedPred);
+                    std::shared_ptr<Rule> combinedRule = std::make_shared<Rule>("combined", rule1->getMinZoom(), rule1->getMaxZoom(), combinedFilter, rule1->getSymbolizers());
+                    it = _rules.erase(it);
+                    *it = combinedRule;
+                    updated = true;
+                    continue;
+                }
+            }
+            it++;
+        }
+
+        if (updated) {
+            rebuildZoomRuleMap();
+        }
+    }
+
+    void Style::rebuildZoomRuleMap() {
+        _zoomRuleMap.clear();
+        _zoomFieldExprsMap.clear();
+        for (auto it = _rules.begin(); it != _rules.end(); it++) {
+            const std::shared_ptr<const Rule>& rule = *it;
+            std::unordered_set<std::shared_ptr<const Expression>> fieldExprs = rule->getReferencedFields();
+            for (int zoom = rule->getMinZoom(); zoom < rule->getMaxZoom(); zoom++) {
+                _zoomRuleMap[zoom].push_back(rule);
+                _zoomFieldExprsMap[zoom].insert(fieldExprs.begin(), fieldExprs.end());
+            }
+        }
     }
 } }
