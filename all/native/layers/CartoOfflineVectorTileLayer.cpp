c3901d7d3911f8d627fcdbbfc3df82e53b2cbd20
Mark Tehver <mtehver@cartodb.com>
2017-07-25 14:41:50 +0300

    Enable tile preloading by default for CartoOfflineVectorTileLayer

--- all/native/layers/CartoOfflineVectorTileLayer.cpp
+++ all/native/layers/CartoOfflineVectorTileLayer.cpp
@@ -9,11 +9,13 @@ namespace carto {
     CartoOfflineVectorTileLayer::CartoOfflineVectorTileLayer(const std::shared_ptr<CartoPackageManager>& packageManager, CartoBaseMapStyle::CartoBaseMapStyle style) :
         CartoVectorTileLayer(std::make_shared<PackageManagerTileDataSource>(packageManager), style)
     {
+        _preloading = true; // turn preloading on by default
     }
 
     CartoOfflineVectorTileLayer::CartoOfflineVectorTileLayer(const std::shared_ptr<CartoPackageManager>& packageManager, const std::shared_ptr<AssetPackage>& styleAssetPackage) :
         CartoVectorTileLayer(std::make_shared<PackageManagerTileDataSource>(packageManager), styleAssetPackage)
     {
+        _preloading = true; // turn preloading on by default
     }
     
     CartoOfflineVectorTileLayer::~CartoOfflineVectorTileLayer() {
