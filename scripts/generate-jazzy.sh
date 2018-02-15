16bd42e3c8e78a7d05cce331d6a4318e42e025e9
Mark Tehver <mtehver@cartodb.com>
2018-02-15 17:02:33 +0200

    Jazzy documentation script cleanup

--- scripts/generate-jazzy.sh
+++ scripts/generate-jazzy.sh
@@ -10,24 +10,11 @@ distDir="${baseDir}/dist/ios"
 rm -rf ${tempDir}
 mkdir -p ${tempDir}
 mkdir -p ${tempDir}/ui
+cp -r ${distDir}/CartoMobileSDK.framework/Headers/CartoMobileSDK.h ${tempDir}/CartoMobileSDK.h
 cp -r ${baseDir}/generated/ios-objc/proxies/* ${tempDir}
-cp -r ${baseDir}/ios/objc/CartoMobileSDK.h ${tempDir}/CartoMobileSDK_base.h
 cp -r ${baseDir}/ios/objc/ui/MapView.h ${tempDir}/ui/MapView.h
 cp -r ${baseDir}/ios/objc/ui/MapView.mm ${tempDir}/ui/MapView.mm
 find ${tempDir} -name "*NTIOSUtils.*" -exec rm {} \;
-
-# Generate umbrella header with additional #define-s
-echo "#define _CARTO_SEARCH_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_GEOCODING_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_ROUTING_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_OFFLINE_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_CUSTOM_BASEMAP_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_PACKAGEMANAGER_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_EDITABLE_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_WKBT_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_VALHALLA_ROUTING_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-echo "#define _CARTO_NMLMODELLODTREE_SUPPORT\n" >> ${tempDir}/CartoMobileSDK.h
-cat ${tempDir}/CartoMobileSDK_base.h >> ${tempDir}/CartoMobileSDK.h
 
 # Execute Jazzy
 rm -rf ${distDir}/docObjC
