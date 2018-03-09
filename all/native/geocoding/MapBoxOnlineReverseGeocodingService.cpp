ea00ecd0981182a2bd714f07f697734b0df929de
Mark Tehver <mtehver@cartodb.com>
2018-03-09 16:14:44 +0200

    MapBox online geocoding service: moved ‘types’ to URL, so that custom types can be specified using custom IRL.

--- all/native/geocoding/MapBoxOnlineReverseGeocodingService.cpp
+++ all/native/geocoding/MapBoxOnlineReverseGeocodingService.cpp
@@ -63,8 +63,6 @@ namespace carto {
 
             baseURL = GeneralUtils::ReplaceTags(_serviceURL.empty() ? MAPBOX_SERVICE_URL : _serviceURL, tagMap);
 
-            params["types"] = "address,poi";
-
             if (!_language.empty()) {
                 params["language"] = _language;
             }
@@ -88,7 +86,7 @@ namespace carto {
         return MapBoxGeocodingProxy::ReadResponse(responseString, request->getProjection());
     }
 
-    const std::string MapBoxOnlineReverseGeocodingService::MAPBOX_SERVICE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/{query}.json?access_token={access_token}";
+    const std::string MapBoxOnlineReverseGeocodingService::MAPBOX_SERVICE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/{query}.json?access_token={access_token}&types=address,poi";
 }
 
 #endif
