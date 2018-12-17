a61562119dbc9063cb52b94d462dea75f1278ada
mtehver <mark.tehver@gmail.com>
2018-12-17 12:58:31 +0200

    Try to use min/maxzoom level from MBTiles metadata table, if present. This avoids slow table scanning in MBTilesTileDataSource constructor.

--- all/native/datasources/MBTilesTileDataSource.cpp
+++ all/native/datasources/MBTilesTileDataSource.cpp
@@ -5,6 +5,7 @@
 #include "core/MapTile.h"
 #include "components/Exceptions.h"
 #include "projections/Projection.h"
+#include "utils/Const.h"
 #include "utils/Log.h"
 
 #include <boost/lexical_cast.hpp>
@@ -25,15 +26,43 @@ namespace carto {
             throw FileException("Failed to open database file", path);
         }
 
+        // First try to use metadata table for min/maxzoom values
+        bool foundMinZoom = false, foundMaxZoom = false;
         try {
-            sqlite3pp::query query(*_db, "SELECT MIN(zoom_level), MAX(zoom_level) FROM tiles");
+            sqlite3pp::query query(*_db, "SELECT name, value FROM metadata WHERE name IN ('minzoom', 'maxzoom')");
             for (auto it = query.begin(); it != query.end(); it++) {
-                _minZoom = it->get<int>(0);
-                _maxZoom = it->get<int>(1);
+                std::string name = (*it).get<const char*>(0);
+                std::string strValue = (*it).get<const char*>(1);
+                int numValue = (strValue.substr(0, 3) == "inf" ? Const::MAX_SUPPORTED_ZOOM_LEVEL : boost::lexical_cast<int>(strValue));
+                if (name == "minzoom") {
+                    _minZoom = numValue;
+                    foundMinZoom = true;
+                } else if (name == "maxzoom") {
+                    _maxZoom = numValue;
+                    foundMaxZoom = true;
+                }
             }
             query.finish();
-        } catch (...) {
-            Log::Error("MBTilesTileDataSource: Failed to read min/max zoom info.");
+        } catch (const std::exception& ex) {
+            Log::Errorf("MBTilesTileDataSource: Exception while reading zoom level metadata: %s", ex.what());
+        }
+
+        // If either was not found, do table scan
+        if (!foundMinZoom || !foundMaxZoom) {
+            try {
+                sqlite3pp::query query(*_db, "SELECT MIN(zoom_level), MAX(zoom_level) FROM tiles");
+                for (auto it = query.begin(); it != query.end(); it++) {
+                    if (!foundMinZoom) {
+                        _minZoom = it->get<int>(0);
+                    }
+                    if (!foundMaxZoom) {
+                        _maxZoom = it->get<int>(1);
+                    }
+                }
+                query.finish();
+            } catch (const std::exception& ex) {
+                Log::Errorf("MBTilesTileDataSource: Failed to read min/max zoom info: %s", ex.what());
+            }
         }
     }
 
@@ -65,10 +94,10 @@ namespace carto {
         if (_db) {
             try {
                 if (_db->disconnect() != SQLITE_OK) {
-                    Log::Error("MBTilesTileDataSource: Failed to close database.");
+                    Log::Error("MBTilesTileDataSource: Failed to close database");
                 }
-            } catch (const std::exception& e) {
-                Log::Errorf("MBTilesTileDataSource: Failed to close database: %s.", e.what());
+            } catch (const std::exception& ex) {
+                Log::Errorf("MBTilesTileDataSource: Failed to close database: %s", ex.what());
             }
             _db.reset();
         }
@@ -77,7 +106,7 @@ namespace carto {
     std::map<std::string, std::string> MBTilesTileDataSource::getMetaData() const {
         std::lock_guard<std::mutex> lock(_mutex);
         if (!_db) {
-            Log::Error("MBTilesTileDataSource::getMetaData: Not connected to the database.");
+            Log::Error("MBTilesTileDataSource::getMetaData: Not connected to the database");
             return std::map<std::string, std::string>();
         }
         
@@ -90,8 +119,8 @@ namespace carto {
             }
             query.finish();
             return metaData;
-        } catch (const std::exception& e) {
-            Log::Errorf("MBTilesTileDataSource::getMetaData: Failed to query metadata from the database: %s.", e.what());
+        } catch (const std::exception& ex) {
+            Log::Errorf("MBTilesTileDataSource::getMetaData: Failed to query metadata from the database: %s", ex.what());
             return std::map<std::string, std::string>();
         }
     }
@@ -99,7 +128,7 @@ namespace carto {
     MapBounds MBTilesTileDataSource::getDataExtent() const {
         std::lock_guard<std::mutex> lock(_mutex);
         if (!_db) {
-            Log::Error("MBTilesTileDataSource::getDataExtent: Not connected to the database.");
+            Log::Error("MBTilesTileDataSource::getDataExtent: Not connected to the database");
             return MapBounds();
         }
 
@@ -108,58 +137,64 @@ namespace carto {
             return *_cachedDataExtent;
         }
         
-        // As a first step, try to use meta data
-        sqlite3pp::query query(*_db, "SELECT value FROM metadata WHERE name='bounds'");
-        for (auto it = query.begin(); it != query.end(); it++) {
-            std::string bounds = (*it).get<const char*>(0);
-            std::vector<std::string> coordinates;
-            boost::split(coordinates, bounds, boost::is_any_of(","));
-            if (coordinates.size() == 4) {
-                double x0 = boost::lexical_cast<double>(boost::trim_copy(coordinates[0]));
-                double y0 = boost::lexical_cast<double>(boost::trim_copy(coordinates[1]));
-                double x1 = boost::lexical_cast<double>(boost::trim_copy(coordinates[2]));
-                double y1 = boost::lexical_cast<double>(boost::trim_copy(coordinates[3]));
-                MapBounds mapBounds;
-                mapBounds.expandToContain(_projection->fromWgs84(MapPos(x0, y0)));
-                mapBounds.expandToContain(_projection->fromWgs84(MapPos(x1, y0)));
-                mapBounds.expandToContain(_projection->fromWgs84(MapPos(x1, y1)));
-                mapBounds.expandToContain(_projection->fromWgs84(MapPos(x0, y1)));
-                _cachedDataExtent.reset(new MapBounds(mapBounds));
-                return mapBounds;
-            }
-        }
-
-        // Meta data not available, use tiles at last zoom level
+        // As a first step, try to use metadata
         MapBounds mapBounds;
+        bool foundMapBounds = false;
         try {
-            sqlite3pp::query query(*_db, "SELECT MIN(tile_column), MIN(tile_row), MAX(tile_column), MAX(tile_row) FROM tiles WHERE zoom_level=:zoom");
-            query.bind(":zoom", _maxZoom);
+            sqlite3pp::query query(*_db, "SELECT value FROM metadata WHERE name='bounds'");
             for (auto it = query.begin(); it != query.end(); it++) {
-                int tileX0 = (*it).get<int>(0);
-                int tileY0 = (*it).get<int>(1);
-                int tileX1 = (*it).get<int>(2) + 1;
-                int tileY1 = (*it).get<int>(3) + 1;
-                if (_scheme == MBTilesScheme::MBTILES_SCHEME_XYZ) { // NOTE: vertically flipped
-                    tileY0 = (1 << _maxZoom) - 1 - tileY0;
-                    tileY1 = (1 << _maxZoom) - 1 - tileY1;
-                    std::swap(tileY0, tileY1);
+                std::string bounds = (*it).get<const char*>(0);
+                std::vector<std::string> coordinates;
+                boost::split(coordinates, bounds, boost::is_any_of(","));
+                if (coordinates.size() == 4) {
+                    double x0 = boost::lexical_cast<double>(boost::trim_copy(coordinates[0]));
+                    double y0 = boost::lexical_cast<double>(boost::trim_copy(coordinates[1]));
+                    double x1 = boost::lexical_cast<double>(boost::trim_copy(coordinates[2]));
+                    double y1 = boost::lexical_cast<double>(boost::trim_copy(coordinates[3]));
+                    mapBounds.expandToContain(_projection->fromWgs84(MapPos(x0, y0)));
+                    mapBounds.expandToContain(_projection->fromWgs84(MapPos(x1, y0)));
+                    mapBounds.expandToContain(_projection->fromWgs84(MapPos(x1, y1)));
+                    mapBounds.expandToContain(_projection->fromWgs84(MapPos(x0, y1)));
+                    foundMapBounds = true;
                 }
-
-                MapBounds projBounds = _projection->getBounds();
-                int zoomTiles = (1 << _maxZoom);
-                double tileWidth  = projBounds.getDelta().getX() / zoomTiles;
-                double tileHeight = projBounds.getDelta().getY() / zoomTiles;
-                
-                MapPos tileP0(projBounds.getMin().getX() + tileX0 * tileWidth, projBounds.getMin().getY() + tileY0 * tileHeight);
-                MapPos tileP1(projBounds.getMin().getX() + tileX1 * tileWidth, projBounds.getMin().getY() + tileY1 * tileHeight);
-                
-                mapBounds.expandToContain(MapBounds(tileP0, tileP1));
             }
-        } catch (const std::exception& e) {
-            Log::Errorf("MBTilesTileDataSource::getDataExtent: Failed to query tile data from the database: %s.", e.what());
-            _cachedDataExtent.reset(new MapBounds());
-            return MapBounds();
+            query.finish();
+        } catch (const std::exception& ex) {
+            Log::Errorf("MBTilesTileDataSource::getDataExtent: Exception while reading bounds metadata: %s", ex.what());
         }
+
+        // If metadata was not available, use tiles at last zoom level
+        if (!foundMapBounds) {
+            try {
+                sqlite3pp::query query(*_db, "SELECT MIN(tile_column), MIN(tile_row), MAX(tile_column), MAX(tile_row) FROM tiles WHERE zoom_level=:zoom");
+                query.bind(":zoom", _maxZoom);
+                for (auto it = query.begin(); it != query.end(); it++) {
+                    int tileX0 = (*it).get<int>(0);
+                    int tileY0 = (*it).get<int>(1);
+                    int tileX1 = (*it).get<int>(2) + 1;
+                    int tileY1 = (*it).get<int>(3) + 1;
+                    if (_scheme == MBTilesScheme::MBTILES_SCHEME_XYZ) { // NOTE: vertically flipped
+                        tileY0 = (1 << _maxZoom) - 1 - tileY0;
+                        tileY1 = (1 << _maxZoom) - 1 - tileY1;
+                        std::swap(tileY0, tileY1);
+                    }
+
+                    MapBounds projBounds = _projection->getBounds();
+                    int zoomTiles = (1 << _maxZoom);
+                    double tileWidth  = projBounds.getDelta().getX() / zoomTiles;
+                    double tileHeight = projBounds.getDelta().getY() / zoomTiles;
+                    
+                    MapPos tileP0(projBounds.getMin().getX() + tileX0 * tileWidth, projBounds.getMin().getY() + tileY0 * tileHeight);
+                    MapPos tileP1(projBounds.getMin().getX() + tileX1 * tileWidth, projBounds.getMin().getY() + tileY1 * tileHeight);
+                    
+                    mapBounds.expandToContain(MapBounds(tileP0, tileP1));
+                }
+                query.finish();
+            } catch (const std::exception& ex) {
+                Log::Errorf("MBTilesTileDataSource::getDataExtent: Failed to query tile data from the database: %s", ex.what());
+            }
+        }
+
         _cachedDataExtent.reset(new MapBounds(mapBounds));
         return mapBounds;
     }
@@ -168,7 +203,7 @@ namespace carto {
         std::lock_guard<std::mutex> lock(_mutex);
         Log::Infof("MBTilesTileDataSource::loadTile: Loading %s", mapTile.toString().c_str());
         if (!_db) {
-            Log::Errorf("MBTilesTileDataSource::loadTile: Failed to load %s: Couldn't connect to the database.", mapTile.toString().c_str());
+            Log::Errorf("MBTilesTileDataSource::loadTile: Failed to load %s: Couldn't connect to the database", mapTile.toString().c_str());
             return std::shared_ptr<TileData>();
         }
         
@@ -183,10 +218,10 @@ namespace carto {
             if (it == query.end()) {
                 std::shared_ptr<TileData> tileData = std::make_shared<TileData>(std::shared_ptr<BinaryData>());
                 if (mapTile.getZoom() > getMinZoom()) {
-                    Log::Infof("MBTilesTileDataSource::loadTile: Tile data doesn't exist in the database, redirecting to parent.");
+                    Log::Infof("MBTilesTileDataSource::loadTile: Tile data doesn't exist in the database, redirecting to parent");
                     tileData->setReplaceWithParent(true);
                 } else {
-                    Log::Infof("MBTilesTileDataSource::loadTile: Tile data doesn't exist in the database.");
+                    Log::Infof("MBTilesTileDataSource::loadTile: Tile data doesn't exist in the database");
                     return std::shared_ptr<TileData>();
                 }
                 return tileData;
@@ -198,8 +233,8 @@ namespace carto {
             query.finish();
     
             return std::make_shared<TileData>(data);
-        } catch (const std::exception& e) {
-            Log::Errorf("MBTilesTileDataSource::loadTile: Failed to query tile data from the database: %s.", e.what());
+        } catch (const std::exception& ex) {
+            Log::Errorf("MBTilesTileDataSource::loadTile: Failed to query tile data from the database: %s", ex.what());
             return std::shared_ptr<TileData>();
         }
     }
