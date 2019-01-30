a958d104166541c0ab039020605d5bb07d2ccc6b
mtehver <mark.tehver@gmail.com>
2019-01-30 12:18:16 +0200

    Fix iOS-specific obscure linking issues

--- all/native/utils/Const.cpp
+++ all/native/utils/Const.cpp
@@ -11,12 +11,13 @@ namespace carto {
     const double Const::LOGE_2 = std::log(2.0);
     const double Const::SQRT_2 = std::sqrt(2.0);
     
-    const float Const::HALF_WORLD_SIZE = Const::WORLD_SIZE / 2.0f;
+    const int Const::WORLD_SIZE = 1 << 20;
+    const float Const::HALF_WORLD_SIZE = (1 << 20) / 2.0f; // WORLD_SIZE / 2.0f
 
     const float Const::UNSCALED_DPI = 160.0f;
 
     const float Const::MIN_SUPPORTED_TILT_ANGLE = 30.0f;
-    const float Const::MAX_HEIGHT = Const::HALF_WORLD_SIZE / 833.0f;
+    const float Const::MAX_HEIGHT = (1 << 20) / 2.0f / 833.0f; // HALF_WORLD_SIZE / 833.0f
     const float Const::MIN_NEAR = 0.25f;
     const float Const::MAX_NEAR = 100.0f;
     
--- all/native/utils/Const.h
+++ all/native/utils/Const.h
@@ -24,7 +24,7 @@ namespace carto {
 
         // World size. World bounds are from left -HALF_WORLD_SIZE to right +HALF_WORLD_SIZE
         // and from top +HALF_WORLD_SIZE to bottom -HALF_WORLD_SIZE
-        static const int WORLD_SIZE = 1 << 20;
+        static const int WORLD_SIZE;
         static const float HALF_WORLD_SIZE;
     
         // Maximum supported zoom level
