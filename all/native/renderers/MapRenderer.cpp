0158564adf2a27fce64513fd86c6070685fce761
mtehver <mark.tehver@gmail.com>
2019-04-04 17:27:55 +0300

    Fix border case handling in moveToFitBounds

--- all/native/renderers/MapRenderer.cpp
+++ all/native/renderers/MapRenderer.cpp
@@ -342,6 +342,9 @@ namespace carto {
                 cglib::vec3<double> maxPos = projectionSurface->calculatePosition(mapBounds.getMax());
                 cglib::mat4x4<double> transform = projectionSurface->calculateTranslateMatrix(minPos, maxPos, 0.5f);
                 centerPos = cglib::transform_point(minPos, transform);
+                if (std::isnan(cglib::norm(centerPos))) {
+                    centerPos = cglib::vec3<double>(0, 0, 0);
+                }
             }
             
             // Adjust the camera tilt, rotation and position to the final state of this animation
