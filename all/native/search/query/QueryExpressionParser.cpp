2fc0172a54476629adda63c9bc5f89774173fe9b
mtehver <mark.tehver@gmail.com>
2020-03-18 16:12:33 +0200

    Minor cleanup

--- all/native/search/query/QueryExpressionParser.cpp
+++ all/native/search/query/QueryExpressionParser.cpp
@@ -16,14 +16,13 @@
 
 namespace carto {
     namespace queryexpressionimpl {
-        namespace phx = boost::phoenix;
-        namespace qi = boost::spirit::qi;
-        namespace repo = boost::spirit::repository::qi;
-        namespace encoding = boost::spirit::iso8859_1;
+        using Skipper = boost::spirit::iso8859_1::space_type;
     
         template <typename Iterator>
-        struct Grammar : qi::grammar<Iterator, std::shared_ptr<QueryExpression>(), encoding::space_type> {
+        struct Grammar : boost::spirit::qi::grammar<Iterator, std::shared_ptr<QueryExpression>(), Skipper> {
             Grammar() : Grammar::base_type(expression) {
+                using namespace boost;
+                using namespace boost::spirit;
                 using qi::_val;
                 using qi::_1;
                 using qi::_2;
@@ -32,27 +31,27 @@ namespace carto {
                               ("\\r", '\r')("\\t", '\t')("\\v", '\v')("\\\\", '\\')
                               ("\\\'", '\'')("\\\"", '\"');
 
-                is_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["is"]];
-                or_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["or"]];
-                and_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["and"]];
-                not_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["not"]];
-                null_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["null"]];
-                false_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["false"]];
-                true_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["true"]];
-                collate_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["collate"]];
-                regexp_like_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["regexp_like"]];
-                regexp_ilike_kw = repo::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["regexp_ilike"]];
+                is_kw    = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["is"]];
+                or_kw    = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["or"]];
+                and_kw   = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["and"]];
+                not_kw   = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["not"]];
+                null_kw  = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["null"]];
+                false_kw = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["false"]];
+                true_kw  = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["true"]];
+                collate_kw = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["collate"]];
+                regexp_like_kw  = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["regexp_like"]];
+                regexp_ilike_kw = repository::qi::distinct(qi::char_("a-zA-Z0-9_"))[qi::no_case["regexp_ilike"]];
 
                 string =
                     '\'' >> *(unesc_char | "\\x" >> qi::hex | (qi::char_ - '\'')) >> '\'';
 
                 value =
-                      null_kw       [_val = phx::construct<Value>()]
-                    | false_kw      [_val = phx::construct<Value>(false)]
-                    | true_kw       [_val = phx::construct<Value>(true)]
-                    | qi::long_long [_val = phx::construct<Value>(_1)]
-                    | qi::double_   [_val = phx::construct<Value>(_1)]
-                    | string        [_val = phx::construct<Value>(_1)]
+                      null_kw       [_val = phoenix::construct<Value>()]
+                    | false_kw      [_val = phoenix::construct<Value>(false)]
+                    | true_kw       [_val = phoenix::construct<Value>(true)]
+                    | qi::long_long [_val = phoenix::construct<Value>(_1)]
+                    | qi::double_   [_val = phoenix::construct<Value>(_1)]
+                    | string        [_val = phoenix::construct<Value>(_1)]
                     ;
 
                 identifier %=
@@ -60,61 +59,61 @@ namespace carto {
                     ;
 
                 expression =
-                    term1 [_val = _1] >> *((("||" | or_kw) > term1) [_val = phx::bind(&OrExpression::create, _val, _1)])
+                    term1 [_val = _1] >> *((("||" | or_kw) > term1) [_val = phoenix::bind(&OrExpression::create, _val, _1)])
                     ;
 
                 term1 =
-                    term2 [_val = _1] >> *((("&&" | and_kw) > term1) [_val = phx::bind(&AndExpression::create, _val, _1)])
+                    term2 [_val = _1] >> *((("&&" | and_kw) > term1) [_val = phoenix::bind(&AndExpression::create, _val, _1)])
                     ;
 
                 term2 =
                       term3 [_val = _1]
-                    | (("!" || not_kw) >> term3) [_val = phx::bind(&NotExpression::create, _1)]
+                    | (("!" || not_kw) >> term3) [_val = phoenix::bind(&NotExpression::create, _1)]
                     ;
 
                 term3 =
                       predicate [_val = _1]
-                    | (regexp_like_kw  >> '(' > operand > ',' > operand > ')')  [_val = phx::bind(&BinaryPredicateExpression<RegexpLikePredicate<false>>::create, _1, _2)]
-                    | (regexp_ilike_kw >> '(' > operand > ',' > operand > ')')  [_val = phx::bind(&BinaryPredicateExpression<RegexpLikePredicate<true>>::create, _1, _2)]
+                    | (regexp_like_kw  >> '(' > operand > ',' > operand > ')')  [_val = phoenix::bind(&BinaryPredicateExpression<RegexpLikePredicate<false>>::create, _1, _2)]
+                    | (regexp_ilike_kw >> '(' > operand > ',' > operand > ')')  [_val = phoenix::bind(&BinaryPredicateExpression<RegexpLikePredicate<true>>::create, _1, _2)]
                     | ('(' >> expression > ')') [_val = _1]
                     ;
 
                 predicate =
-                      (operand >> "<=" > operand) [_val = phx::bind(&BinaryPredicateExpression<LtePredicate>::create, _1, _2)]
-                    | (operand >> ">=" > operand) [_val = phx::bind(&BinaryPredicateExpression<GtePredicate>::create, _1, _2)]
-                    | (operand >> "<"  > operand) [_val = phx::bind(&BinaryPredicateExpression<LtPredicate>::create, _1, _2)]
-                    | (operand >> ">"  > operand) [_val = phx::bind(&BinaryPredicateExpression<GtPredicate>::create, _1, _2)]
-                    | (operand >> (qi::lit("==") | qi::lit("=") ) > operand) [_val = phx::bind(&BinaryPredicateExpression<EqPredicate>::create, _1, _2)]
-                    | (operand >> (qi::lit("<>") | qi::lit("!=")) > operand) [_val = phx::bind(&BinaryPredicateExpression<NeqPredicate>::create, _1, _2)]
-                    | (operand >> is_kw >> null_kw)           [_val = phx::bind(&UnaryPredicateExpression<IsNullPredicate>::create, _1)]
-                    | (operand >> is_kw >> not_kw >> null_kw) [_val = phx::bind(&UnaryPredicateExpression<IsNotNullPredicate>::create, _1)]
+                      (operand >> "<=" > operand) [_val = phoenix::bind(&BinaryPredicateExpression<LtePredicate>::create, _1, _2)]
+                    | (operand >> ">=" > operand) [_val = phoenix::bind(&BinaryPredicateExpression<GtePredicate>::create, _1, _2)]
+                    | (operand >> "<"  > operand) [_val = phoenix::bind(&BinaryPredicateExpression<LtPredicate>::create, _1, _2)]
+                    | (operand >> ">"  > operand) [_val = phoenix::bind(&BinaryPredicateExpression<GtPredicate>::create, _1, _2)]
+                    | (operand >> (qi::lit("==") | qi::lit("=") ) > operand) [_val = phoenix::bind(&BinaryPredicateExpression<EqPredicate>::create, _1, _2)]
+                    | (operand >> (qi::lit("<>") | qi::lit("!=")) > operand) [_val = phoenix::bind(&BinaryPredicateExpression<NeqPredicate>::create, _1, _2)]
+                    | (operand >> is_kw >> null_kw)           [_val = phoenix::bind(&UnaryPredicateExpression<IsNullPredicate>::create, _1)]
+                    | (operand >> is_kw >> not_kw >> null_kw) [_val = phoenix::bind(&UnaryPredicateExpression<IsNotNullPredicate>::create, _1)]
                     ;
         
                 operand =
-                      value [_val = phx::bind(&ConstOperand::create, _1)]
-                    | (identifier >> collate_kw > identifier) [_val = phx::bind(&VariableOperand::createEx, _1, _2)]
-                    | identifier [_val = phx::bind(&VariableOperand::create, _1)]
+                      value [_val = phoenix::bind(&ConstOperand::create, _1)]
+                    | (identifier >> collate_kw > identifier) [_val = phoenix::bind(&VariableOperand::createEx, _1, _2)]
+                    | identifier [_val = phoenix::bind(&VariableOperand::create, _1)]
                     ;
             }
 
-            qi::symbols<char const, char const> unesc_char;
-            qi::rule<Iterator, qi::unused_type()> is_kw, or_kw, and_kw, not_kw, null_kw, false_kw, true_kw, collate_kw, regexp_like_kw, regexp_ilike_kw;
-            qi::rule<Iterator, std::string()> string;
-            qi::rule<Iterator, Value()> value;
-            qi::rule<Iterator, std::string()> identifier;
-            qi::rule<Iterator, std::shared_ptr<Expression>(), encoding::space_type> expression, term1, term2, term3, predicate;
-            qi::rule<Iterator, std::shared_ptr<Operand>(), encoding::space_type> operand;
+            boost::spirit::qi::symbols<char const, char const> unesc_char;
+            boost::spirit::qi::rule<Iterator, boost::spirit::qi::unused_type()> is_kw, or_kw, and_kw, not_kw, null_kw, false_kw, true_kw, collate_kw, regexp_like_kw, regexp_ilike_kw;
+            boost::spirit::qi::rule<Iterator, std::string()> string;
+            boost::spirit::qi::rule<Iterator, Value()> value;
+            boost::spirit::qi::rule<Iterator, std::string()> identifier;
+            boost::spirit::qi::rule<Iterator, std::shared_ptr<Expression>(), Skipper> expression, term1, term2, term3, predicate;
+            boost::spirit::qi::rule<Iterator, std::shared_ptr<Operand>(), Skipper> operand;
         };
     }
 
     std::shared_ptr<QueryExpression> QueryExpressionParser::parse(const std::string& expr) {
         std::string::const_iterator it = expr.begin();
         std::string::const_iterator end = expr.end();
-        queryexpressionimpl::encoding::space_type space;
         std::shared_ptr<QueryExpression> queryExpr;
         bool result = false;
         try {
-            result = boost::spirit::qi::phrase_parse(it, end, queryexpressionimpl::Grammar<std::string::const_iterator>(), space, queryExpr);
+            queryexpressionimpl::Skipper skipper;
+            result = boost::spirit::qi::phrase_parse(it, end, queryexpressionimpl::Grammar<std::string::const_iterator>(), skipper, queryExpr);
         }
         catch (const boost::spirit::qi::expectation_failure<std::string::const_iterator>& ex) {
             throw ParseException("Expectation error", expr, static_cast<int>(ex.first - expr.begin()));
