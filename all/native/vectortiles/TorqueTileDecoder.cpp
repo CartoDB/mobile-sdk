b4c55a0af61eea07344ad17e6f44672d055cfcdd
mtehver <mark.tehver@gmail.com>
2021-05-24 18:34:01 +0300

    Another fix

--- all/native/vectortiles/TorqueTileDecoder.cpp
+++ all/native/vectortiles/TorqueTileDecoder.cpp
@@ -20,7 +20,7 @@
 namespace carto {
     
     TorqueTileDecoder::TorqueTileDecoder(const std::shared_ptr<CartoCSSStyleSet>& styleSet) :
-        _logger(std::make_shared<MapnikVTLogger>("TorqueTileDecoder")),
+        _logger(std::make_shared<MVTLogger>("TorqueTileDecoder")),
         _fallbackFonts(),
         _map(),
         _mapSettings(),
@@ -156,7 +156,7 @@ namespace carto {
         auto strokeMap = std::make_shared<vt::StrokeMap>(1, 1);
         auto glyphMap = std::make_shared<vt::GlyphMap>(GLYPHMAP_SIZE, GLYPHMAP_SIZE);
 
-        std::shared_ptr<vt::Font> fallbackFont;
+        std::shared_ptr<const vt::Font> fallbackFont;
         for (auto it = _fallbackFonts.rbegin(); it != _fallbackFonts.rend(); it++) {
             std::shared_ptr<BinaryData> fontData = *it;
             std::string fontName = fontManager->loadFontData(*fontData->getDataPtr());
