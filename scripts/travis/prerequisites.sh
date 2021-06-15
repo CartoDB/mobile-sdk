bb74f408cd1410d14324bd18fd866477942b9029
mtehver <mark.tehver@gmail.com>
2021-06-15 15:24:03 +0300

    Travis script updates

--- scripts/travis/prerequisites.sh
+++ scripts/travis/prerequisites.sh
@@ -2,6 +2,9 @@ set -e
 
 rvm get head
 
+echo '---- Updating submodules ----'
+git submodule update --init --remote --recursive
+
 echo '---- Setting up boost ----'
 cd libs-external/boost
 ./bootstrap.sh
@@ -9,17 +12,17 @@ cd libs-external/boost
 cd ../..
 
 echo '---- Downloading and installing CMake ----'
-curl -o cmake-3.5.2.tar.gz -L https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz
-rm -rf cmake-3.5.2
-tar xpfz cmake-3.5.2.tar.gz
-cd cmake-3.5.2
+curl -o cmake-3.10.2.tar.gz -L https://cmake.org/files/v3.10/cmake-3.10.2.tar.gz
+rm -rf cmake-3.10.2
+tar xpfz cmake-3.10.2.tar.gz
+cd cmake-3.10.2
 ./configure --prefix=`pwd`/dist
 make
 make install
 export PATH=$PWD/dist/bin:$PATH
 cd ..
 
-echo '---- Downloading and installing Swig ----'
+echo '---- Downloading and installing SWIG ----'
 rm -rf mobile-swig
 git clone https://github.com/CartoDB/mobile-swig.git
 cd mobile-swig
@@ -38,11 +41,11 @@ export PATH=$PWD/dist/bin:$PATH
 cd ..
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  echo '---- Downloading and installing Android NDK r12b ----'
-  curl -L http://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip -O
-  rm -r -f android-ndk-r12b
-  unzip -q android-ndk-r12b-linux-x86_64.zip
-  rm android-ndk-r12b-linux-x86_64.zip
-  export ANDROID_NDK_HOME=`pwd`/android-ndk-r12b;
+  echo '---- Downloading and installing Android NDK r22b ----'
+  curl -L https://dl.google.com/android/repository/android-ndk-r22b-linux-x86_64.zip -O
+  rm -r -f android-ndk-r22b
+  unzip -q android-ndk-r22b-linux-x86_64.zip
+  rm android-ndk-r22b-linux-x86_64.zip
+  export ANDROID_NDK_HOME=`pwd`/android-ndk-r22b;
 #  export ANDROID_HOME=/usr/local/android-sdk
 fi
