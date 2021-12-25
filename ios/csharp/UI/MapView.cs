96c59f9bacfcc4aa55423f431daacc2b21e68e26
mtehver <mark.tehver@gmail.com>
2021-12-25 18:01:42 +0200

    Sync Xamarin.iOS MapView implementation with ObjC implementation

--- ios/csharp/UI/MapView.cs
+++ ios/csharp/UI/MapView.cs
@@ -17,21 +17,17 @@
     /// </summary>
     [Foundation.Register("MapView")]
     public partial class MapView : GLKView {
-        private EAGLContext _viewContext;
         private bool _active = false;
+        private bool _surfaceCreated = false;
+        private CGSize _activeDrawableSize = new CGSize(0, 0);
         private float _scale = 1;
+        private NSObject _willResignActiveNotificationObserver;
         private NSObject _didEnterBackgroundNotificationObserver;
         private NSObject _willEnterForegroundNotificationObserver;
         private MapRedrawRequestListener _redrawRequestListener;
 
         private UITouch _pointer1 = null;
         private UITouch _pointer2 = null;
-
-        private class MapViewGLKViewDelegate : GLKViewDelegate {
-            public override void DrawInRect(GLKView view, CGRect rect) {
-                ((MapView) view).OnDraw();
-            }
-        }
 
         static MapView() {
             IOSUtils.InitializeLog();
@@ -73,14 +69,11 @@
         }
 
         private void InitBase() {
-            this.Delegate = new MapViewGLKViewDelegate();
-
-            _didEnterBackgroundNotificationObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidEnterBackgroundNotification, OnAppDidEnterBackground, null);
-            _willEnterForegroundNotificationObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.WillEnterForegroundNotification, OnAppWillEnterForeground, null);
-
-            _active = true;
-
+            _active = false;
+            _surfaceCreated = false;
+            _activeDrawableSize = new CGSize(0, 0);
             _scale = (float) UIScreen.MainScreen.NativeScale;
+            ContentScaleFactor = _scale;
 
             _baseMapView = new BaseMapView();
             _baseMapView.GetOptions().DPI = 160 * _scale;
@@ -88,111 +81,170 @@
             _redrawRequestListener = new MapRedrawRequestListener(this);
             _baseMapView.SetRedrawRequestListener(_redrawRequestListener);
 
-            _viewContext = new EAGLContext(EAGLRenderingAPI.OpenGLES2);
-            if (_viewContext == null) {
-                Log.Fatal("MapView.InitGL: Failed to create OpenGLES 2.0 context");
+            if (Window != null) {
+                InitContext();
+                _active = true;
+                SetNeedsDisplay();
             }
-            this.Context = _viewContext;
-            this.ContentScaleFactor = _scale;
 
+            _willResignActiveNotificationObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.WillResignActiveNotification, OnAppWillResignActive, null);
+            _didEnterBackgroundNotificationObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidEnterBackgroundNotification, OnAppDidEnterBackground, null);
+            _willEnterForegroundNotificationObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.WillEnterForegroundNotification, OnAppWillEnterForeground, null);
+        }
+
+        private void InitContext() {
+            var context = new EAGLContext(EAGLRenderingAPI.OpenGLES2);
+            if (context == null) {
+                Log.Fatal("MapView.InitContext: Failed to create OpenGL ES 2.0 context");
+            }
+
+            Context = context;
+            MultipleTouchEnabled = true;
             DrawableColorFormat = GLKViewDrawableColorFormat.RGBA8888;
             DrawableDepthFormat = GLKViewDrawableDepthFormat.Format24;
             DrawableMultisample = GLKViewDrawableMultisample.None;
             DrawableStencilFormat = GLKViewDrawableStencilFormat.Format8;
-            MultipleTouchEnabled = true;
+        }
 
-            if (_viewContext != null) {
-                EAGLContext.SetCurrentContext(_viewContext);
-                _baseMapView.OnSurfaceCreated();
-                _baseMapView.OnSurfaceChanged((int) (Bounds.Size.Width * _scale), (int) (Bounds.Size.Height * _scale));
+        public override void WillMoveToWindow(UIWindow newWindow) {
+            base.WillMoveToWindow(newWindow);
+
+            if (newWindow == null) {
+                Log.Info("MapView.WillMoveToWindow: null");
+            } else {
+                Log.Info("MapView.WillMoveToWindow: nonnull");
+                lock (this) {
+                    if (!_active) {
+                        InitContext();
+                        if (EAGLContext.CurrentContext == null) {
+                            EAGLContext.SetCurrentContext(Context);
+                        }
+                        _active = true;
+                        _surfaceCreated = false;
+                    }
+                }
             }
+        }
+
+        public override void MovedToWindow() {
+            base.MovedToWindow();
+
+            if (Window == null) {
+                Log.Info("MapView.MovedToWindow: null");
+
+                lock (this) {
+                    if (_active) {
+                        _baseMapView.OnSurfaceDestroyed();
+                        if (EAGLContext.CurrentContext == Context) {
+                            EAGLContext.SetCurrentContext(null);
+                        }
+                        _active = false;
+                        _surfaceCreated = false;
+                    }
+                }
+            } else {
+                Log.Info("MapView.MovedToWindow: nonnull");
+
+                SetNeedsDisplay();
+            }
+        }
+
+        public override void LayoutSubviews() {
+            base.LayoutSubviews();
 
             SetNeedsDisplay();
         }
 
-        private void OnDraw() {
+        public override void Draw(CGRect rect) {
             lock (this) {
-                if (_viewContext != null && _active) {
-                    EAGLContext context = EAGLContext.CurrentContext;
-                    if (context != _viewContext) {
-                        EAGLContext.SetCurrentContext(_viewContext);
+                if (_active) {
+                    if (!_surfaceCreated) {
+                        _baseMapView.OnSurfaceCreated();
+                        _activeDrawableSize = new CGSize(0, 0);
+                        _surfaceCreated = true;
                     }
+
+                    float drawableWidth = DrawableWidth;
+                    float drawableHeight = DrawableHeight;
+                    if (_activeDrawableSize.Width != drawableWidth || _activeDrawableSize.Height != drawableHeight) {
+                        _activeDrawableSize = new CGSize(drawableWidth, drawableHeight);
+                        _baseMapView.OnSurfaceChanged((int)_activeDrawableSize.Width, (int)_activeDrawableSize.Height);
+                    }
+
                     _baseMapView.OnDrawFrame();
-                    if (context != _viewContext) {
-                        EAGLContext.SetCurrentContext(context);
-                    }
                 }
             }
-        }
-
-        private void OnAppDidEnterBackground(NSNotification notification) {
-            lock (this) {
-                _active = false;
-                if (_viewContext != null) {
-                    EAGLContext context = EAGLContext.CurrentContext;
-                    if (context != _viewContext) {
-                        EAGLContext.SetCurrentContext(_viewContext);
-                    }
-                    _baseMapView.FinishRendering();
-                    if (context != _viewContext) {
-                        EAGLContext.SetCurrentContext(context);
-                    }
-                }
-            }
-        }
-
-        private void OnAppWillEnterForeground(NSNotification notification) {
-            lock (this) {
-                _active = true;
-            }
-            SetNeedsDisplay();
         }
 
         protected override void Dispose(bool disposing) {
+            if (_willResignActiveNotificationObserver != null) {
+                NSNotificationCenter.DefaultCenter.RemoveObserver(_willResignActiveNotificationObserver);
+                _willResignActiveNotificationObserver = null;
+            }
+            if (_didEnterBackgroundNotificationObserver != null) {
+                NSNotificationCenter.DefaultCenter.RemoveObserver(_didEnterBackgroundNotificationObserver);
+                _didEnterBackgroundNotificationObserver = null;
+            }
+            if (_willEnterForegroundNotificationObserver != null) {
+                NSNotificationCenter.DefaultCenter.RemoveObserver(_willEnterForegroundNotificationObserver);
+                _willEnterForegroundNotificationObserver = null;
+            }
+
             lock (this) {
                 if (_redrawRequestListener != null) {
                     _redrawRequestListener.Detach();
                     _redrawRequestListener = null;
                 }
                 if (_baseMapView != null) {
-                    _baseMapView.SetRedrawRequestListener(null);
                     _baseMapView.OnSurfaceDestroyed();
+                    _baseMapView.SetRedrawRequestListener(null);
                     _baseMapView = null;
+                    _surfaceCreated = false;
+                    _activeDrawableSize = new CGSize(0, 0);
                 }
-                if (_viewContext != null) {
-                    if (EAGLContext.CurrentContext == _viewContext) {
-                        EAGLContext.SetCurrentContext(null);
-                    }
-                    _viewContext = null;
-                }
-                if (_didEnterBackgroundNotificationObserver != null) {
-                    NSNotificationCenter.DefaultCenter.RemoveObserver(_didEnterBackgroundNotificationObserver);
-                    _didEnterBackgroundNotificationObserver = null;
-                }
-                if (_willEnterForegroundNotificationObserver != null) {
-                    NSNotificationCenter.DefaultCenter.RemoveObserver(_willEnterForegroundNotificationObserver);
-                    _willEnterForegroundNotificationObserver = null;
+                if (EAGLContext.CurrentContext == Context) {
+                    EAGLContext.SetCurrentContext(null);
                 }
             }
 
             base.Dispose(disposing);
         }
 
-        public override void LayoutSubviews() {
-            base.LayoutSubviews();
+        private void OnAppWillResignActive(NSNotification notification) {
+            Log.Info("MapView.OnAppWillResignActive");
 
-            lock (this) {
-                if (_viewContext != null) {
+            lock (this){
+                if (_active) {
                     EAGLContext context = EAGLContext.CurrentContext;
-                    if (context != _viewContext) {
-                        EAGLContext.SetCurrentContext(_viewContext);
+                    if (context != Context) {
+                        EAGLContext.SetCurrentContext(Context);
                     }
-                    _baseMapView.OnSurfaceChanged((int) (Bounds.Size.Width * _scale), (int) (Bounds.Size.Height * _scale));
-                    if (context != _viewContext) {
+
+                    _baseMapView.FinishRendering();
+
+                    if (context != Context) {
                         EAGLContext.SetCurrentContext(context);
                     }
                 }
+                _active = false;
             }
+        }
+
+        private void OnAppDidEnterBackground(NSNotification notification) {
+            Log.Info("MapView.OnAppDidEnterBackground");
+
+            lock (this) {
+                _active = false;
+            }
+        }
+
+        private void OnAppWillEnterForeground(NSNotification notification) {
+            Log.Info("MapView.OnAppWillEnterForeground");
+
+            lock (this) {
+                _active = true;
+            }
+
             SetNeedsDisplay();
         }
 
